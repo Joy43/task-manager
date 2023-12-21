f6c1f899a14ce417f5f686e5c1e4a97fe655338e
joy43 <ssjoy43@gmail.com>
2023-12-21 23:50:46 +0600

    adding some point

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyAyA2QgGUE6rEUrVjCOGvS4Ypg4F_O_HqQ",
+  authDomain: "task-management-platform-42b4c.firebaseapp.com",
+  projectId: "task-management-platform-42b4c",
+  storageBucket: "task-management-platform-42b4c.appspot.com",
+  messagingSenderId: "432234799464",
+  appId: "1:432234799464:web:e632c09533d7acc7c036dd",
+  measurementId: "G-8YWDZ6TTQK"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
